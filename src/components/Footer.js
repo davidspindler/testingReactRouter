6e5731929bfe49282490f4b1942c03ba85058ff2
Alex <aspindle@ucsd.edu>
2021-12-23 14:44:04 -0800

    some error made build fail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faYoutube, faVk, faGithub, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 
