e76b43311b91f245edbe74c8888abbcb16cb76ac
Alex <aspindle@ucsd.edu>
2021-12-27 18:57:29 -0800

    using react-ga

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import './App.css';
+import { useEffect } from 'react';
 import Home from './pages/Home';
 import Resources from './pages/Resources';
 import Blog from './pages/Blog';
 import Donate from './pages/Donate';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactGa from 'react-ga';
 
 
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('G-WC5ZWP4P3N');
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+
+
   return (
     <Router>
       <Routes>
