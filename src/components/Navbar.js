4f29daf40ec139a934b87811a2dc8d09a2ee82d6
Alex <aspindle@ucsd.edu>
2021-12-27 20:49:45 -0800

    working nav responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,84 @@
 import { Link } from 'react-router-dom';
 import logo from '../../src/logo.png';
+import React, {useState, useEffect} from 'react';
 
 function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    let alreadyOpen = false;
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
+        
+
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+
+        }
+
+        window.addEventListener('resize',changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    }, [])
+
+
     return (
         <>
 
-        <nav class="navbar">
+        <nav id="myTopnav" class="navbar">
             <div class="container">
            
             <img src={logo} className="logo" alt="Spindler Academy logo"/>
-            <ul class="nav-links">
-                <li class="nav-item">
-                    <a href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <Link to="/blog">Blog</Link>
-                </li>
-                <li class="nav-item">
-                    <Link to="/resources">Resources</Link>
-                </li>
-                <li class="nav-item">
-                    <Link to="/donate">Donate</Link>
-                </li>
-                <li class="nav-item">
-                    <a href="#links">Links</a>
-                </li>
-            </ul>
+
+            {(screenWidth > 768 ) && (
+                <ul class="nav-links">
+                    <li class="nav-item">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/resources">Resources</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/donate">Donate</Link>
+                    </li>
+                    
+                </ul>
+            )}
+            
+
+            <span onClick={toggleNav} class="navTrigger" >
+                    <i></i>
+                    <i></i>
+                    <i></i>
+            </span>
             </div>
         </nav>
+
+        {(toggleMenu && screenWidth < 768) && (
+                <ul class="nav-links">
+                    <li class="nav-item">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/resources">Resources</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/donate">Donate</Link>
+                    </li>
+                    
+                </ul>
+            )}
 
         </>
     )
