eaa2fe4e1497502436106346cdb71d288404bbec
Alex <aspindle@ucsd.edu>
2021-12-31 21:33:24 -0800

    add 2022 goals

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -9,6 +9,8 @@ export default function Blog() {
         <a href="https://www.youtube.com/watch?v=CIjnzXxtM-A">Lord of the Most Dangerous Hunger Games</a>
         <br />
         <a href="https://docs.google.com/document/d/1_Mu392CzJ7lmZg-PQ3br3ZxXzCbIN6y3To-EAXGBVlE/edit?usp=sharing">Math in Rome</a>
+        <br />
+        <a href="https://docs.google.com/document/d/1Xu0NMSasV6-XCsrdHGqUaokclvnWWDViLrzdO1kQwfk/edit?usp=sharing">2022 Goals</a>
         </>
     )
 }
