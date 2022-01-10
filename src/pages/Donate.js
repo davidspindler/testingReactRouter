7dae198bc4a6e3c4d78f15ebe1b15ac99c2189ee
Alex <aspindle@ucsd.edu>
2022-01-09 21:51:41 -0800

    added btc address & some links in resources

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -8,9 +8,14 @@ export default function Donate() {
         <h1>Donate</h1>
         <div id="donate">
         
-        <p>If you would like to support me, my DOGE address is:</p><br />
-        <p>DFQhAMEZUKZUkuRyJ4Nz19EESDkVHi4uqx</p>
+        <p>If you would like to support me, my addresses are:</p><br />
+        
 
+        <h2>BTC:</h2> <br />
+        
+        <p>bc1quzt4pmjnk7v2046jx9tmjn5fkv2zdqffr9khqc</p>
+        <h2>DOGE:</h2> <br />
+        <p>DFQhAMEZUKZUkuRyJ4Nz19EESDkVHi4uqx</p>
         </div>
         </>
     )
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -14,14 +14,22 @@ export default function Resources() {
             TI 84 Calculator ROM</a>*/}
         
         <br />
-        <img src={mag} className="mag" alt="magnet link"/> 
         <a href="magnet:?xt=urn:btih:12d12d35a45e2142b6a698c1b9a8447768e712d5&dn=SpindlerAcademyExampleVideo.mkv&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://tracker.openbittorrent.com:6969">
-            Example Spindler Academy video</a> 
-        <br />
-        <img src={mag} className="mag" alt="magnet link"/>
-        <a href="magnet:?xt=urn:btih:964b8e675e118920dc92c148ee2a9526d9dba32e&dn=HowToTorrentFinal.mpeg&tr=http://tracker.bt4g.com:2095/announce&tr=udp://tracker.openbittorrent.com:6969&tr=udp://exodus.desync.com:6969/announce&tr=udp://retracker.hotplug.ru:2710/announce&tr=https://tracker.loligirl.cn:443/announce&tr=udp://code2chicken.nl:6969/announce&tr=http://t.nyaatracker.com:80/announce&tr=udp://open.xxtor.com:3074/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=http://buny.uk:6969/announce&tr=udp://tracker.moeking.eu.org:6969/announce&tr=udp://tracker.beeimg.com:6969/announce&tr=https://tr.torland.ga:443/announce&tr=https://tracker.iriseden.fr:443/announce&tr=https://t.btcland.xyz:443/announce&tr=udp://tracker.cyberia.is:6969/announce&tr=udp://tracker.moeking.me:6969/announce&tr=udp://tracker.dix.tf:6969/announce&tr=https://tr.burnabyhighstar.com:443/announce&tr=https://tr.highstar.shop:443/announce&tr=udp://tracker.birkenwald.de:6969/announce&tr=http://tracker.ipv6tracker.ru:80/announce&tr=https://trackme.theom.nz:443/announce&tr=https://t1.tokhmi.xyz:443/announce&tr=udp://www.torrent.eu.org:451/announce&tr=https://tracker.nanoha.org:443/announce&tr=https://tracker.kuroy.me:443/announce&tr=udp://tracker6.lelux.fi:6969/announce&tr=udp://tracker.sylphix.com:6969/announce&tr=https://tr.fuckbitcoin.xyz:443/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.theoks.net:6969/announce&tr=udp://ipv6.tracker.monitorit4.me:6969/announce&tr=https://tr.doogh.club:443/announce&tr=udp://bt.oiyo.tk:6969/announce&tr=https://tracker.lilithraws.cf:443/announce">
-            How To Torrent 
+            <img src={mag} className="mag" alt="magnet link"/>
         </a>
+        <a href="https://www.youtube.com/watch?v=_j-sUfeqNUU">
+            <img src={tub} className="tub" alt="youtube link"/>
+        </a>
+           Example Spindler Academy video 
+        <br />
+        
+        <a href="magnet:?xt=urn:btih:964b8e675e118920dc92c148ee2a9526d9dba32e&dn=HowToTorrentFinal.mpeg&tr=http://tracker.bt4g.com:2095/announce&tr=udp://tracker.openbittorrent.com:6969&tr=udp://exodus.desync.com:6969/announce&tr=udp://retracker.hotplug.ru:2710/announce&tr=https://tracker.loligirl.cn:443/announce&tr=udp://code2chicken.nl:6969/announce&tr=http://t.nyaatracker.com:80/announce&tr=udp://open.xxtor.com:3074/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=http://buny.uk:6969/announce&tr=udp://tracker.moeking.eu.org:6969/announce&tr=udp://tracker.beeimg.com:6969/announce&tr=https://tr.torland.ga:443/announce&tr=https://tracker.iriseden.fr:443/announce&tr=https://t.btcland.xyz:443/announce&tr=udp://tracker.cyberia.is:6969/announce&tr=udp://tracker.moeking.me:6969/announce&tr=udp://tracker.dix.tf:6969/announce&tr=https://tr.burnabyhighstar.com:443/announce&tr=https://tr.highstar.shop:443/announce&tr=udp://tracker.birkenwald.de:6969/announce&tr=http://tracker.ipv6tracker.ru:80/announce&tr=https://trackme.theom.nz:443/announce&tr=https://t1.tokhmi.xyz:443/announce&tr=udp://www.torrent.eu.org:451/announce&tr=https://tracker.nanoha.org:443/announce&tr=https://tracker.kuroy.me:443/announce&tr=udp://tracker6.lelux.fi:6969/announce&tr=udp://tracker.sylphix.com:6969/announce&tr=https://tr.fuckbitcoin.xyz:443/announce&tr=udp://tracker.torrent.eu.org:451/announce&tr=udp://tracker.theoks.net:6969/announce&tr=udp://ipv6.tracker.monitorit4.me:6969/announce&tr=https://tr.doogh.club:443/announce&tr=udp://bt.oiyo.tk:6969/announce&tr=https://tracker.lilithraws.cf:443/announce">
+            <img src={mag} className="mag" alt="magnet link"/>
+        </a>
+        <a href="https://www.youtube.com/watch?v=XCMPke6Wrgc">
+            <img src={tub} className="tub" alt="youtube link"/>
+        </a>
+            How To Torrent 
         <br />
         <img src={tub} className="tub" alt="youtube link"/>
         <a href="https://www.youtube.com/playlist?list=PL-SStBoAJuw0vj8MgTFhY5y9wSFnjGbOB">
@@ -32,7 +40,11 @@ export default function Resources() {
         <a href="https://www.youtube.com/playlist?list=PLxQSlOe-wlgAG_A_6ES9hpD_2-gm1D-UY">
             Machine Learning (UCSD)
         </a>
-
+        <br />
+        <img src={tub} className="tub" alt="youtube link"/>
+        <a href="https://www.youtube.com/playlist?list=PL1y1iaEtjSYiiSGVlL1cHsXN_kvJOOhu-">
+            Web Security (Stanford)
+        </a>
         </>
     )
 }
