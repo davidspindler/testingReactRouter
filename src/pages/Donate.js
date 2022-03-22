2068bcd123344431bce0826759c98b6a4fac0f88
Alex <aspindle@ucsd.edu>
2022-03-22 16:33:48 -0700

    change donation page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -5,14 +5,15 @@ export default function Donate() {
     return (
         <>
         <Link to="/">Back</Link>
-        <h1>Donate</h1>
+        <h1>Donate (actually Gift)</h1>
         <div id="donate">
         
-        <p>If you would like to support me, my addresses are:</p><br />
+        <p>According to my rudimentary search, I must distinguish that you are not making a donation to me, and rather a gift to me. The difference 
+            being that you can not write it off on your taxes. Considering I'm only accepting cryptocurrency, I'm not sure if this applies; but I wanted to 
+            state it anyway. If you would still like to support me, my addresses are:</p><br />
         
 
         <h2>BTC:</h2> <br />
-        
         <p>bc1quzt4pmjnk7v2046jx9tmjn5fkv2zdqffr9khqc</p>
         <h2>DOGE:</h2> <br />
         <p>DFQhAMEZUKZUkuRyJ4Nz19EESDkVHi4uqx</p>
