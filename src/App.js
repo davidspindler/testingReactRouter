6d256962d2a1081ff522a263a6c01348d51a384d
David <davidspindler612@gmail.com>
2022-03-23 08:26:29 -0700

    fixed uppercase

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
+import "./App.css";
+import { useEffect } from "react";
+import Home from "./pages/Home";
+import Resources from "./pages/Resources";
+import Blog from "./pages/Blog";
+import Donate from "./pages/Donate";
 
-import './App.css';
-import { useEffect } from 'react';
-import Home from './pages/Home';
-import Resources from './pages/Resources';
-import Blog from './pages/Blog';
-import Donate from './pages/Donate';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ReactGa from 'react-ga';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactGa from "react-ga";
 
 function App() {
-
   useEffect(() => {
-    ReactGa.initialize('G-WC5ZWP4P3N');
-    ReactGa.pageview(window.location.pathname + window.location.search)
-  }, [])
-
+    ReactGa.initialize("G-WC5ZWP4P3N");
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/Resources" element={<Resources />}></Route>
-        <Route path="/Blog" element={<Blog />}></Route>
-        <Route path="/Donate" element={<Donate />}></Route>
+        <Route path="/resources" element={<Resources />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/donate" element={<Donate />}></Route>
       </Routes>
-      <div >
-      
-      </div>
+      <div></div>
     </Router>
-    
-    
   );
 }
 
